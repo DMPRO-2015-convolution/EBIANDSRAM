6470c59d28fca605c1ec4246086e14295e617795
Truls Rustad Fossum <trulfos@gmail.com>
2015-11-19 22:38:10 +0100

    Start project

---
+++ src/Camvolution.vhd
@@ -0,0 +1,32 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity Camvolution is
+	port (
+		sys_clk : in std_logic;
+		led : out std_logic;
+		sw6, sw7 : in std_logic;
+		
+		sram1_address : out std_logic_vector(18 downto 0);
+		sram1_data : inout std_logic_vector(15 downto 0);
+		sram1_ce, sram1_oe, sram1_lb, sram1_ub, sram1_we : out std_logic;
+		
+		sram2_address : out std_logic_vector(18 downto 0);
+		sram2_data : inout std_logic_vector(15 downto 0);
+		sram2_ce, sram2_oe, sram2_lb, sram2_ub, sram2_we : out std_logic;
+
+		ebi_wen, ebi_ren, ebi_cs0, ebi_cs1 : in std_logic;
+		ebi_address : in std_logic_vector(19 downto 0);
+		ebi_data : inout std_logic_vector(15 downto 0)
+	);
+end Camvolution;
+
+architecture Behavioral of Camvolution is
+
+begin
+
+	memory_manager : entity work.MemoryManager port map (
+	);
+
+end Behavioral;
+
---
+++ src/MemoryManager.vhd
@@ -0,0 +1,18 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+
+entity MemoryManager is
+	port (
+		clk : in std_logic;
+		efm_mode : in boolean;
+		ebi_data : in std_logic_vector(15 downto 0)
+end MemoryManager;
+
+architecture Behavioral of MemoryManager is
+
+begin
+
+
+end Behavioral;
+
